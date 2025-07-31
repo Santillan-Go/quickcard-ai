26f7b3eba75333716ddcd64f4fffb22026d8f51b
Santillan-C <santillango10405@gmail.com>
2025-07-31 12:50:17 -0700

    making some improvement with pagebook for the eighth time

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,6 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6335035765993896"
+        crossorigin="anonymous"
+      ></script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
